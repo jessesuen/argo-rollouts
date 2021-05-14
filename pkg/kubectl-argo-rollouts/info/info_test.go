ae54edae62bc7e5eef6c3bfcc1ea44cb22fdee05
Jesse Suen <jesse_suen@intuit.com>
2021-05-14 15:53:34 -0700

    refactor: move rollout phase caluclation to library
    Signed-off-by: Jesse Suen <jesse_suen@intuit.com>

--- pkg/kubectl-argo-rollouts/info/info_test.go
+++ pkg/kubectl-argo-rollouts/info/info_test.go
@@ -1,7 +1,6 @@
 package info
 
 import (
-	"strconv"
 	"testing"
 	"time"
 
@@ -174,18 +173,6 @@ func TestExperimentInfo(t *testing.T) {
 	})
 }
 
-func TestRolloutStatusDegraded(t *testing.T) {
-	ro := newCanaryRollout()
-	ro.Status.Conditions = append(ro.Status.Conditions, v1alpha1.RolloutCondition{
-		Type:    v1alpha1.RolloutProgressing,
-		Reason:  "ProgressDeadlineExceeded",
-		Message: "timed out",
-	})
-	status, message := RolloutStatusString(ro)
-	assert.Equal(t, "Degraded", status)
-	assert.Equal(t, "ProgressDeadlineExceeded: timed out", message)
-}
-
 func TestRolloutStatusInvalidSpec(t *testing.T) {
 	rolloutObjs := testdata.NewInvalidRollout()
 	roInfo := NewRolloutInfo(rolloutObjs.Rollouts[0], rolloutObjs.ReplicaSets, rolloutObjs.Pods, rolloutObjs.Experiments, rolloutObjs.AnalysisRuns)
@@ -198,156 +185,4 @@ func TestRolloutAborted(t *testing.T) {
 	roInfo := NewRolloutInfo(rolloutObjs.Rollouts[0], rolloutObjs.ReplicaSets, rolloutObjs.Pods, rolloutObjs.Experiments, rolloutObjs.AnalysisRuns)
 	assert.Equal(t, "Degraded", roInfo.Status)
 	assert.Equal(t, `RolloutAborted: metric "web" assessed Failed due to failed (1) > failureLimit (0)`, roInfo.Message)
-}
-
-func TestRolloutStatusPaused(t *testing.T) {
-	ro := newCanaryRollout()
-	ro.Spec.Paused = true
-	status, message := RolloutStatusString(ro)
-	assert.Equal(t, "Paused", status)
-	assert.Equal(t, "manually paused", message)
-}
-
-func TestRolloutStatusProgressing(t *testing.T) {
-	{
-		ro := newCanaryRollout()
-		ro.Spec.Replicas = pointer.Int32Ptr(5)
-		ro.Status.UpdatedReplicas = 4
-		ro.Status.AvailableReplicas = 4
-		ro.Status.Replicas = 5
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Progressing", status)
-		assert.Equal(t, "more replicas need to be updated", message)
-	}
-	{
-		ro := newCanaryRollout()
-		ro.Spec.Replicas = pointer.Int32Ptr(5)
-		ro.Status.UpdatedReplicas = 5
-		ro.Status.AvailableReplicas = 4
-		ro.Status.Replicas = 5
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Progressing", status)
-		assert.Equal(t, "updated replicas are still becoming available", message)
-	}
-	{
-		ro := newCanaryRollout()
-		ro.Spec.Replicas = pointer.Int32Ptr(5)
-		ro.Status.UpdatedReplicas = 5
-		ro.Status.AvailableReplicas = 5
-		ro.Status.Replicas = 7
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Progressing", status)
-		assert.Equal(t, "old replicas are pending termination", message)
-	}
-	{
-		ro := newBlueGreenRollout()
-		ro.Status.BlueGreen.ActiveSelector = "abc1234"
-		ro.Status.StableRS = "abc1234"
-		ro.Status.CurrentPodHash = "def5678"
-		ro.Spec.Replicas = pointer.Int32Ptr(5)
-		ro.Status.Replicas = 5
-		ro.Status.UpdatedReplicas = 5
-		ro.Status.AvailableReplicas = 5
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Progressing", status)
-		assert.Equal(t, "active service cutover pending", message)
-	}
-	{
-		ro := newBlueGreenRollout()
-		ro.Status.BlueGreen.ActiveSelector = "def5678"
-		ro.Status.StableRS = "abc1234"
-		ro.Status.CurrentPodHash = "def5678"
-		ro.Spec.Replicas = pointer.Int32Ptr(5)
-		ro.Status.Replicas = 5
-		ro.Status.UpdatedReplicas = 5
-		ro.Status.AvailableReplicas = 5
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Progressing", status)
-		assert.Equal(t, "waiting for analysis to complete", message)
-	}
-	{
-		// Scenario when a newly created rollout has partially filled in status (with hashes)
-		// but no updated replica count
-		ro := newCanaryRollout()
-		ro.Spec.Replicas = nil
-		ro.Status = v1alpha1.RolloutStatus{
-			ObservedGeneration: strconv.Itoa(int(ro.Generation)),
-			StableRS:           "abc1234",
-			CurrentPodHash:     "abc1234",
-		}
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Progressing", status)
-		assert.Equal(t, "more replicas need to be updated", message)
-	}
-	{
-		// Rollout observed generation is not updated
-		ro := newCanaryRollout()
-		ro.Generation = 2
-		ro.Spec.Replicas = nil
-		ro.Status = v1alpha1.RolloutStatus{
-			StableRS:           "abc1234",
-			CurrentPodHash:     "abc1234",
-			ObservedGeneration: "1",
-		}
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Progressing", status)
-		assert.Equal(t, "waiting for rollout spec update to be observed", message)
-	}
-	{
-		// Make sure we skip isGenerationObserved check when rollout is a v0.9 legacy rollout using
-		// a hash and not a numeric observed generation
-		ro := newCanaryRollout()
-		ro.Generation = 2
-		ro.Spec.Replicas = nil
-		ro.Status = v1alpha1.RolloutStatus{
-			StableRS:           "abc1234",
-			CurrentPodHash:     "abc1234",
-			ObservedGeneration: "7d66d4485f",
-		}
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Progressing", status)
-		assert.Equal(t, "more replicas need to be updated", message)
-	}
-	{
-		// Verify isGenerationObserved detects a v0.9 legacy rollout which has an all numeric hash
-		ro := newCanaryRollout()
-		ro.Generation = 2
-		ro.Spec.Replicas = nil
-		ro.Status = v1alpha1.RolloutStatus{
-			StableRS:           "abc1234",
-			CurrentPodHash:     "abc1234",
-			ObservedGeneration: "1366344857",
-		}
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Progressing", status)
-		assert.Equal(t, "more replicas need to be updated", message)
-	}
-}
-
-func TestRolloutStatusHealthy(t *testing.T) {
-	{
-		ro := newCanaryRollout()
-		ro.Status.Replicas = 5
-		ro.Status.UpdatedReplicas = 5
-		ro.Status.AvailableReplicas = 5
-		ro.Status.ReadyReplicas = 5
-		ro.Status.StableRS = "abc1234"
-		ro.Status.CurrentPodHash = "abc1234"
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Healthy", status)
-		assert.Equal(t, "", message)
-	}
-	{
-		ro := newBlueGreenRollout()
-		ro.Status.Replicas = 5
-		ro.Status.UpdatedReplicas = 5
-		ro.Status.AvailableReplicas = 5
-		ro.Status.ReadyReplicas = 5
-		ro.Status.BlueGreen.ActiveSelector = "abc1234"
-		ro.Status.CurrentPodHash = "abc1234"
-		ro.Status.StableRS = "abc1234"
-		status, message := RolloutStatusString(ro)
-		assert.Equal(t, "Healthy", status)
-		assert.Equal(t, "", message)
-	}
 }
